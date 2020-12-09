56aa711105081c80010925bbfe86beca2b4788fa
tt <96486d9b@gmail.com>
2020-12-09 10:59:43 +0800

    feat: github action

--- main.py
+++ main.py
@@ -6,61 +6,63 @@ import sys
 
 
 class task():
-    def __init__(self):
-        pass
-
-    def hello(self, ecard_username: str, ecard_password: str, dingding_token: str, dingding_secret: str, delta=600):
-        data = ecard(
-            username=ecard_username,
-            password=ecard_password,
-            delta=delta
-        )
-        name = data['name']
+    @staticmethod
+    def hello(ecard_username: str, ecard_password: str, dingding_token: str, dingding_secret: str):
         dingding(
             title='Hello World!',
-            text='Hello {}, GitHub Action OK!'.format(name),
+            text='Hello {}, GitHub Action OK!'.format(ecard_username),
             token=dingding_token,
             secret=dingding_secret
         )
+        task.run(
+            ecard_username=ecard_username,
+            ecard_password=ecard_password,
+            dingding_token=dingding_token,
+            dingding_secret=dingding_secret
+        )
 
-    def run(self, ecard_username: str, ecard_password: str, dingding_token: str, dingding_secret: str, delta=600):
+    @staticmethod
+    def run(ecard_username: str, ecard_password: str, dingding_token: str, dingding_secret: str):
         data = ecard(
             username=ecard_username,
-            password=ecard_password,
-            delta=delta
+            password=ecard_password
         )
-        balance = data['balance']
-        total_cost = data['total_cost']
-        for item in data['recent_cost']:
+        text = '***\n\n'
+        text += '{} 消费 {:.2f} 元 余额 {:.2f} 元'.format(data['date'], -1 * data['total_cost'], data['balance'])
+        for item in data['items'][::-1]:
             occtime = item['occtime']
             mercname = item['mercname']
             tranname = item['tranname']
             sign_tranamt = int(item['sign_tranamt']) / 100
-            text = "ecard monitor\n\n{}-{}-{} {}:{}:{}\n\n{} {}\n\n消费 {:.2f} 元 剩余 {:.2f} 元 累计 {:.2f} 元".format(
+            text += '\n\n***\n\n'
+            text += "{}-{}-{} {}:{}:{}\n\n{} {} 消费 {:.2f} 元".format(
                 occtime[0:4], occtime[4:6], occtime[6:8],
                 occtime[8:10], occtime[10:12], occtime[12:14], mercname, tranname,
-                -1*sign_tranamt, balance, -1*total_cost
+                -1*sign_tranamt
             )
-            dingding(
-                title="新的校园卡消费信息~",
-                text=text,
-                token=dingding_token,
-                secret=dingding_secret
-            )
+        text += '\n\n***'
+        dingding(
+            title="昨日校园卡消费信息~",
+            text=text,
+            token=dingding_token,
+            secret=dingding_secret
+        )
+
 
 if __name__ == "__main__":
-    obj = task()
     if sys.argv[1] == 'hello':
-        obj.hello(
+        task.hello(
+            ecard_username=os.getenv('ECARD_USERNAME'),
+            ecard_password=os.getenv('ECARD_PASSWORD'),
+            dingding_token=os.getenv('DINGDING_TOKEN'),
+            dingding_secret=os.getenv('DINGDING_SECRET')
+        )
+    elif sys.argv[1] == 'run':
+        task.run(
             ecard_username=os.getenv('ECARD_USERNAME'),
             ecard_password=os.getenv('ECARD_PASSWORD'),
             dingding_token=os.getenv('DINGDING_TOKEN'),
             dingding_secret=os.getenv('DINGDING_SECRET')
         )
     else:
-        obj.run(
-            ecard_username=os.getenv('ECARD_USERNAME'),
-            ecard_password=os.getenv('ECARD_PASSWORD'),
-            dingding_token=os.getenv('DINGDING_TOKEN'),
-            dingding_secret=os.getenv('DINGDING_SECRET')
-        )
+        pass
--- readme.md
+++ readme.md
@@ -2,11 +2,15 @@
 
 ## Pre
 
-> 申请钉钉机器人，参考https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq。安全设置为加签方式，记该密钥为secret。申请得到的Webhook地址里提取access_token字段，记为token。
+> 申请钉钉机器人，参考[钉钉自定义机器人文档](https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq)。
+
+> 机器人安全设置为加签方式，提取该密钥secret。
+
+> 申请得到的Webhook地址中提取access_token字段。
 
 ## Run
 
-> Fork该项目。选择Setting→Secret→New repository secret。分别创建以下字段。
+> Fork该项目。选择Setting→Secret→New repository secret，创建以下字段。
 
 ```
 ECARD_USERNAME=你的校园卡账号
@@ -15,41 +19,64 @@ DINGDING_TOKEN=申请的钉钉机器人access_token
 DINGDING_SECRET=申请的钉钉机器人secret
 ```
 
-> 选择Action→Set up a workflow yourself。以下提供一个Action Example。
+> 选择Action→Set up a workflow yourself，新建一个Workflow，以下为Action YML示例。
 
 ```yml
-name: Monitor Your Card
-
+name: monitor your card
+ 
 on:
+  push:
+    branches: [ master ]
   schedule:
-    # run every 10 minutes
-    - cron:  '*/10 * * * *'
+    - cron:  '0 0 * * *'
 
 jobs:
-  monitor-your-card:
-    name: monitor your card
+  monitor:
     runs-on: ubuntu-latest
-    steps:
-      - name: checkout actions
-      - uses: actions/checkout@v1
 
-      - name: Set up Python 3.7
-      - uses: actions/setup-python@v1
+    steps:
+      - uses: actions/checkout@v2
+      
+      - uses: actions/setup-python@v2
         with:
           python-version: 3.7
 
-      - uses: your_github_username/@master
+      - name: monitor-hello
+        if: ${{ github.event_name == 'push' }}
         env:
           ECARD_USERNAME: ${{ secrets.ECARD_USERNAME }}
           ECARD_PASSWORD: ${{ secrets.ECARD_PASSWORD }}
-          DINGDING_USERNAME: ${{ secrets.DINGDING_USERNAME }}
-          DINGDING_PASSWORD: ${{ secrets.DINGDING_PASSWORD }}
-        if: ${{ github.event_name == 'push' }}
+          DINGDING_TOKEN: ${{ secrets.DINGDING_TOKEN }}
+          DINGDING_SECRET: ${{ secrets.DINGDING_SECRET }}
         run: |
-        pip install -r requirements
-        python main.py hello
+          pip install -r requirements.txt
+          python main.py hello
+        
+      - name: monitor-run
+        if: ${{ github.event_name == 'schedule' }}
+        env:
+          ECARD_USERNAME: ${{ secrets.ECARD_USERNAME }}
+          ECARD_PASSWORD: ${{ secrets.ECARD_PASSWORD }}
+          DINGDING_TOKEN: ${{ secrets.DINGDING_TOKEN }}
+          DINGDING_SECRET: ${{ secrets.DINGDING_SECRET }}
         run: |
-        pip install -r requirements
-        python main.py
+          pip install -r requirements.txt
+          python main.py run
 ```
 
+> Push trigger触发hello函数，配置成功后钉钉会收到配置成功消息和昨日消费信息。
+
+> Schedule trigger触发run函数，每天早上8点推送昨日消费信息。
+
+## Tree
+
+```
+.
+├── main.py           # 程序主入口
+├── monitor           # 监控端
+│   └── api.py        # 监控api，目前实现校园卡
+├── notice            # 通知端
+│   └── api.py        # 通知api，目前支持钉钉&微信
+└── utils             # 工具类
+    └── auth.py       # 登录接口
+```
